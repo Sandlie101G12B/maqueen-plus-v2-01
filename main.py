5c8fc1ac34cf05c6b59d49b573df473163c79eca
Sandile Nkambule <sandilenkambule12345@gmail.com>
2025-05-24 20:01:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,225 +1,75 @@
-# MakeCode-compatible imports
-from microbit import i2c, pin13, pin14, pin15, display, running_time, sleep
-import neopixel
-
-# Use MakeCode's neopixel.create API
-neo_pixel = neopixel.create(pin15, 4)
-
-def pause(ms):
-    sleep(ms)
-
-# Constants
-I2C_ADDR = 0x10
-VERSION_COUNT_I2C_ADDR = 0x32
-VERSION_DATA_I2C_ADDR = 0x33
-LEFT_MOTOR_I2C_ADDR = 0x00
-AXLE_WIDTH = 0.095
-FORWARD = 0
-BACKWARD = 1
-
-# Ultrasonic Rangefinder
-US_TRIGGER = pin13
-US_ECHO = pin14
-MIN_DISTANCE = 2
-MAX_DISTANCE = 450
-MAX_DURATION = 38000
-SPEED_OF_SOUND = 343.4 * 100 / 1000000  # cm/us
-
-# NeoPixel
-NEO_PIXEL_PIN = pin15
-# Already created above: neo_pixel = neopixel.create(NEO_PIXEL_PIN, 4)
-
-# IR sensor constants for version 2.1
-LINE_SENSOR_I2C_ADDR = 0x1D
-ANALOG_L2_I2C_ADDR = 0x26
-ANALOG_L1_I2C_ADDR = 0x24
-ANALOG_M_I2C_ADDR = 0x22
-ANALOG_R1_I2C_ADDR = 0x20
-ANALOG_R2_I2C_ADDR = 0x1E
-ALL_ANALOG_SENSOR_I2C_ADDRS = [ANALOG_L2_I2C_ADDR, ANALOG_L1_I2C_ADDR, ANALOG_M_I2C_ADDR, ANALOG_R1_I2C_ADDR, ANALOG_R2_I2C_ADDR]
-sensor_index = [4, 3, 2, 1, 0]
-L2 = 0
-L1 = 1
-M = 2
-R1 = 3
-R2 = 4
-DIGITAL_SENSOR_STATUS_I2C_ADDR = 0x1D
-DIGITAL_SENSOR_MASK = [16, 8, 4, 2, 1]
-DIGITAL_SENSOR_SHIFT = [4, 3, 2, 1, 0]
-
-# LED constants
-LEFT_LED_I2C_ADDR = 0x0B
-RIGHT_LED_I2C_ADDR = 0x0C
-LEFT = 0
-RIGHT = 1
-BOTH = 2
-ON = 1
-OFF = 0
-
-# Servos
-SERVO_1 = 0x14
-SERVO_2 = 0x15
-SERVO_3 = 0x16
-
-# NeoPixel color constants
-RED = 0xFF0000
-ORANGE = 0xFFA500
-YELLOW = 0xFFFF00
-GREEN = 0x00FF00
-BLUE = 0x0000FF
-INDIGO = 0x4B0082
-VIOLET = 0x8A2BE2
-PURPLE = 0xFF00FF
-WHITE = 0xFF9070
-# OFF = 0x000000
-
-# General purpose functions
-def init_maqueen():
-    global sensor_index
-    version = maqueen_version()
-    if version[-3:] == "2.0":
-        sensor_index = [0, 1, 2, 3, 4]
-    elif version[-3:] == "2.1":
-        sensor_index = [4, 3, 2, 1, 0]
-    pause(1000)
-    display.clear()
-
-def eight_bits(n):
-    return max(min(n, 255), 0)
-
-def one_bit(n):
-    return max(min(n, 1), 0)
-
-def maqueen_version():
-    # Use simple integer buffer for MakeCode
-    i2c.write(I2C_ADDR, bytearray([VERSION_COUNT_I2C_ADDR]))
-    count = i2c.read(I2C_ADDR, 1)[0]
-    i2c.write(I2C_ADDR, bytearray([VERSION_DATA_I2C_ADDR]))
-    version = "2.1"  # Simulate version for MakeCode
-    return version
-
-def color_to_rgb(color):
-    r = (color >> 16) & 0xFF
-    g = (color >> 8) & 0xFF
-    b = color & 0xFF
-    return (r, g, b)
-
-# Motor functions
-def stop():
-    drive(0)
-
-def drive(speed_left, speed_right=None):
-    if speed_right is None:
-        speed_right = speed_left
-    motors(speed_left, FORWARD, speed_right, FORWARD)
-
-def backup(speed_left, speed_right=None):
-    if speed_right is None:
-        speed_right = speed_left
-    motors(speed_left, BACKWARD, speed_right, BACKWARD)
-
-def spin_left(speed):
-    motors(speed, BACKWARD, speed, FORWARD)
-
-def spin_right(speed):
-    motors(speed, FORWARD, speed, BACKWARD)
-
-def motors(l_speed, l_direction, r_speed, r_direction):
-    buf = bytearray(5)
-    buf[0] = LEFT_MOTOR_I2C_ADDR
-    buf[1] = one_bit(l_direction)
-    buf[2] = eight_bits(round(l_speed))
-    buf[3] = one_bit(r_direction)
-    buf[4] = eight_bits(round(r_speed))
-    i2c.write(I2C_ADDR, buf)
-
-# IR line sensor functions
-def read_all_line_sensors():
-    values = []
-    for index in sensor_index:
-        i2c.write(I2C_ADDR, bytearray([ALL_ANALOG_SENSOR_I2C_ADDRS[index]]))
-        buffer = i2c.read(I2C_ADDR, 2)
-        values.append((buffer[1] << 8) | buffer[0])
-    return values
-
-def read_line_sensor(sensor):
-    i2c.write(I2C_ADDR, bytearray([ALL_ANALOG_SENSOR_I2C_ADDRS[sensor_index[sensor]]]))
-    buffer = i2c.read(I2C_ADDR, 2)
-    return (buffer[1] << 8) | buffer[0]
-
-def sensor_on_line(sensor):
-    i2c.write(I2C_ADDR, bytearray([DIGITAL_SENSOR_STATUS_I2C_ADDR]))
-    sensor_state = i2c.read(I2C_ADDR, 1)[0]
-    return ((sensor_state & DIGITAL_SENSOR_MASK[sensor]) >> DIGITAL_SENSOR_SHIFT[sensor]) == 1
-
-# LED head light functions
-def headlights(select, state):
-    if select == LEFT:
-        i2c.write(I2C_ADDR, bytearray([LEFT_LED_I2C_ADDR, state]))
-    elif select == RIGHT:
-        i2c.write(I2C_ADDR, bytearray([RIGHT_LED_I2C_ADDR, state]))
-    else:
-        i2c.write(I2C_ADDR, bytearray([LEFT_LED_I2C_ADDR, state, state]))
-
-# Underglow lighting functions
-def underglow_off():
-    set_underglow(OFF)
-
-def set_underglow_light(light, color):
-    neo_pixel[light] = color_to_rgb(color)
-    neo_pixel.show()
-
-def set_servo_angle(servo, angle):
-    angle = max(min(angle, 180), 0)
-    i2c.write(I2C_ADDR, bytearray([servo, angle]))
-
-def rangefinder():
-    # Simulate ultrasonic sensor for MakeCode
-    # Return a fixed value or random value in range
-    return 30  # Simulated 30cm distance
-
-def set_underglow(color):
-    rgb = color_to_rgb(color)
-    for i in range(4):
-        neo_pixel[i] = rgb
-    neo_pixel.show()
-#---------------------------------------------------------------------------------------
-
-
-
-# Initialize
-init_maqueen()
-
-path_memory = []
+from microbit import display, sleep, running_time
+import maqueen
 
 # Constants
 OBSTACLE_THRESHOLD = 25
 SCAN_ANGLE_LEFT = 45
 SCAN_ANGLE_RIGHT = 135
 SCAN_ANGLE_CENTER = 90
+LEFT = 0
+RIGHT = 1
+BOTH = 2  # For headlights logic
+ON = 1
+OFF = 0
 
+path_memory = []
+
+
+def headlights(select, state):
+    if select == LEFT:
+        maqueen.write_led(LEFT, state)
+    elif select == RIGHT:
+        maqueen.write_led(RIGHT, state)
+    else:
+        maqueen.write_led(LEFT, state)
+        maqueen.write_led(RIGHT, state)
+
+def set_servo_angle(servo, angle):
+    maqueen.servo_run(servo, angle)
+
+def stop():
+    maqueen.motor_stop(maqueen.Motors.ALL)
+
+def drive(speed_left, speed_right=None):
+    if speed_right is None:
+        speed_right = speed_left
+    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, speed_left)
+    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, speed_right)
+
+def spin_left(speed):
+    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CCW, speed)
+    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, speed)
+
+def spin_right(speed):
+    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, speed)
+    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CCW, speed)
+
+
+
+def read_line_sensor(sensor):
+    return maqueen.read_patrol(sensor)
+
+def rangefinder():
+    return maqueen.ultrasonic()
 
 def startup():
     display.scroll("Group 38")
     headlights(BOTH, ON)
-    set_servo_angle(SERVO_1, SCAN_ANGLE_CENTER)
-    # Simulate gripper open/close with servo if needed
-    set_servo_angle(SERVO_2, 0)  # Open
-    set_servo_angle(SERVO_2, 90)  # Close
-    # Optionally play a sound here
+    set_servo_angle(maqueen.Servos.S1, SCAN_ANGLE_CENTER)
+    set_servo_angle(maqueen.Servos.S2, 0)  # Open gripper
+    set_servo_angle(maqueen.Servos.S2, 90)  # Close gripper
     display.clear()
-
 
 def follow_line_until_node():
     start_time = running_time()
     while True:
-        left = read_line_sensor(L1)
-        right = read_line_sensor(R1)
-        if left > 150 and right > 150:
+        left = read_line_sensor(maqueen.PatrolSensors.L1)
+        right = read_line_sensor(maqueen.PatrolSensors.R1)
+        if left and right:
             drive(50, 50)
-        elif left > 150:
+        elif left:
             drive(20, 50)
-        elif right > 150:
+        elif right:
             drive(50, 20)
         else:
             stop()
@@ -227,13 +77,12 @@ def follow_line_until_node():
     end_time = running_time()
     return end_time - start_time
 
-
 def scan_for_obstacles():
-    set_servo_angle(SERVO_1, SCAN_ANGLE_LEFT)
+    set_servo_angle(maqueen.Servos.S1, SCAN_ANGLE_LEFT)
     left_dist = rangefinder()
-    set_servo_angle(SERVO_1, SCAN_ANGLE_RIGHT)
+    set_servo_angle(maqueen.Servos.S1, SCAN_ANGLE_RIGHT)
     right_dist = rangefinder()
-    set_servo_angle(SERVO_1, SCAN_ANGLE_CENTER)
+    set_servo_angle(maqueen.Servos.S1, SCAN_ANGLE_CENTER)
     if left_dist > OBSTACLE_THRESHOLD:
         return 'L'
     elif right_dist > OBSTACLE_THRESHOLD:
@@ -241,9 +90,7 @@ def scan_for_obstacles():
     else:
         return 'NONE'
 
-
 def turn_and_log(direction, move_time):
-    # timestamp = running_time()
     path_memory.append((direction, move_time))
     if direction == 'L':
         spin_left(50)
@@ -251,18 +98,14 @@ def turn_and_log(direction, move_time):
     elif direction == 'R':
         spin_right(50)
         stop()
-    # Optionally play a sound here
-
 
 def deliver_package():
     stop()
-    set_servo_angle(SERVO_2, 0)  # Open gripper
-    # Optionally play a sound here
-
+    set_servo_angle(maqueen.Servos.S2, 0)  # Open gripper
 
 def replay_path(reverse=True):
     sequence = list(reversed(path_memory)) if reverse else path_memory
-    for _, direction, move_time in sequence:
+    for direction, move_time in sequence:
         if reverse:
             direction = 'L' if direction == 'R' else 'R'
         if direction == 'L':
@@ -274,13 +117,10 @@ def replay_path(reverse=True):
         drive(50, 50)
         stop()
 
-
 def pickup_package2():
     drive(50, 50)
     stop()
-    set_servo_angle(SERVO_2, 90)  # Close gripper
-    # Optionally play a sound here
-
+    set_servo_angle(maqueen.Servos.S2, 90)  # Close gripper
 
 def shutdown():
     deliver_package()
